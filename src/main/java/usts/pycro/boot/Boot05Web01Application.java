90064dff6a08c3ccffa83804f1f5c3ff992ae9b9
Pycro <2023747015@qq.com>
2023-04-11 18:10:29 +0800

    4.11 commit

--- src/main/java/usts/pycro/boot/Boot05Web01Application.java
+++ src/main/java/usts/pycro/boot/Boot05Web01Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Boot05Web01Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Boot05Web01Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Boot05Web01Application.class, args);
+    }
 
 }
